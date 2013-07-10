05aab031c03d619fcff3ee93254a7cd619b64f70
Adrian Kelly <votingelephant@outlook.ie>
2013-07-10 12:37:13 +0100

    updated index.html and web.js

--- web.js
+++ web.js
@@ -3,15 +3,12 @@
 var express = require('express');
 var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var app = express(express.logger());
 
-var buffer = new Buffer(32);
-
-buffer = fs.readFileSync("index.html");
-buffer = buffer.toString();
+var html_string = fs.readFileSync("index.html", "UTF-8");
 
 app.get('/', function(request, response) {
-  response.send(buffer);
+  response.send(html_string);
 });
 
 var port = process.env.PORT || 5000;
